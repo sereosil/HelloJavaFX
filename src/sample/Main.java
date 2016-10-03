3a88fa9bbcc5c3b4efdd3d680ded49075f6f1e13
sereosil <sereosil@gmail.com>
2016-10-03 15:31:21 +0300

    only list of printers now. returns name of chosen printer;

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -26,12 +26,7 @@ import java.util.Random;
 import static javafx.print.Printer.getAllPrinters;
 
 public class Main extends Application {
-   /* private Random random = new Random();
-    private Label createLabel(){
-        Label label = LabelBuilder.create().text("Some Text").prefWidth(100).prefHeight(50).alignment(Pos.CENTER).layoutX(random.nextInt(windowWidth-100))
-        return label;
-    }*/
-   public void print(Node node) {
+   public String print(Node node) {
        ChoiceDialog dialog = new ChoiceDialog(Printer.getDefaultPrinter(), Printer.getAllPrinters());
        dialog.setHeaderText("Choose the printer!");
        dialog.setContentText("Choose a printer from available printers");
@@ -39,60 +34,19 @@ public class Main extends Application {
        Optional<Printer> opt = dialog.showAndWait();
        if (opt.isPresent()) {
            Printer printer = opt.get();
-          // PageLayout pageLayout = printer.createPageLayout(file,PageOrientation.PORTRAIT, Printer.MarginType.DEFAULT);
-           PrinterJob job = PrinterJob.createPrinterJob();
-           job.setPrinter(printer);
-           if (job != null) {
-               boolean success = job.printPage(node);
-               if (success) {
-                   job.endJob();
-               }
-           }
+           String nameOfPrinter=printer.getName();
+           return nameOfPrinter;
        }
+       return null;
    }
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //primaryStage.setTitle("Hello World");
-       // Group root1 = new Group();
-      //  Scene scene = new Scene(root1, 300, 275);
-        File pdfFile = new File("C:/Users/sereo_000/Downloads/magistratura_1_kurs_4_f-t.pdf");
-
-       //root1.getChildren().add(pdfFile);
-        //Printer.getAllPrinters();
-        //public static ObservableSet<Printer> getAllPrinters();
-        //ChoiceDialog dialog = new ChoiceDialog(Printer.getAllPrinters());
-        Node node = null;
-        final VBox root = new VBox(5);
-        Scene scene1 = new Scene(root);
-        scene1.setUserData(pdfFile);
-        // Add the scene to the Stage
-        primaryStage.setScene(scene1);
-        // Set the title of the Stage
-        primaryStage.setTitle("A Printing Nodes Example");
-        // Display the Stage
-        primaryStage.show();
-        //node.getTransforms().add(pdfFile);
-        print(root);
-       // ObservableSet<Printer> printer = Printer.getAllPrinters();
-       // Button btn = new Button();
-       // btn.setLayoutX(100);
-      //  btn.setLayoutY(80);
-       // btn.setText("Hello World");
-/*
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World");
-            }
-        });
-*/
-       /* root1.getChildren().add(btn);
-        //root1.getChildren().add((Node) printer);
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
-    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Node node=null;
+        print(node);
     }
 }
